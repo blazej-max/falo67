b9236e3d02c76a835b603274f906fe30ae264999
blazej <blazej.tomkowicz07@gmail.com>
2026-04-17 03:22:03 +0200

    fix firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyADm_OEOXZrMksHlYHlwKQurHW5HBTDbc",
+  apiKey: "AIzaSyADm_OEOXZrMksHlYHlWkKQurHW5HBTDbc",
   authDomain: "falo-9525c.firebaseapp.com",
   projectId: "falo-9525c",
   storageBucket: "falo-9525c.firebasestorage.app",
   messagingSenderId: "192168193442",
-  appId: "1:192168193442:web:41fac1a7520823b30816fb",
-  measurementId: "G-Q8QNN3G6HQ"
+  appId: "1:192168193442:web:38fdc81df09f75da0816fb",
+  measurementId: "G-J2ZNPMXNB6"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
