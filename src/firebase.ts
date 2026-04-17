c09dd3d3a36f9f600d7138d73f734a5658f7786e
blazej <blazej.tomkowicz07@gmail.com>
2026-04-17 03:26:50 +0200

    fix firebase auth export

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyADm_OEOXZrMksHlYHlWkKQurHW5HBTDbc",
   authDomain: "falo-9525c.firebaseapp.com",
@@ -16,6 +12,10 @@ const firebaseConfig = {
   measurementId: "G-J2ZNPMXNB6"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// 🔥 TO JEST KLUCZ
+export const auth = getAuth(app);
+
+// analytics (opcjonalne, nie psuje nic)
+export const analytics = getAnalytics(app);
