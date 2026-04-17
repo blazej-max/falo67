4e92fb9c155783b42e753e1a6e63ec71c3c31f0e
blazej <blazej.tomkowicz07@gmail.com>
2026-04-17 02:43:14 +0200

    fix admin route

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/admin/menu" element={<AdminMenu />} />
+          <Route path="/admin" element={<AdminMenu />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
